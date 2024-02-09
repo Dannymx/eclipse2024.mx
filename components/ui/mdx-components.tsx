f6efce1b527180b17469748bf25e16cbac10e334
Dan Saldana <hello@dan.mx>
2024-02-08 21:54:09 -0600

    feat: add page with guidence

--- components/ui/mdx-components.tsx
+++ components/ui/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpRightFromSquareIcon } from "lucide-react";
 import type { MDXComponents } from "mdx/types";
 import type { ImageProps } from "next/image";
 import Image from "next/image";
@@ -25,14 +26,33 @@ export const HomeComponents: MDXComponents = {
 };
 
 export const PageComponents: MDXComponents = {
+  a: ({ className, children, ...props }) => (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <a
+      className={cn("text-xl mb-4 inline-block hover:underline", className)}
+      {...props}
+    >
+      {children}
+      <ArrowUpRightFromSquareIcon size={20} className="mx-1 inline-block" />
+    </a>
+  ),
   p: ({ className, ...props }) => (
     <p className={cn("text-xl mb-4", className)} {...props} />
   ),
   h3: ({ className, ...props }) => (
     <h3 className={cn("text-4xl font-bebas-neue", className)} {...props} />
   ),
+  h4: ({ className, ...props }) => (
+    <h4 className={cn("text-3xl font-bebas-neue", className)} {...props} />
+  ),
   img: (props) => (
     // eslint-disable-next-line jsx-a11y/alt-text
     <Image style={{ objectFit: "cover" }} {...(props as ImageProps)} fill />
+  ),
+  ul: ({ className, ...props }) => (
+    <ul
+      className={cn("text-xl list-decimal list-inside mb-4", className)}
+      {...props}
+    />
   ),
 };
